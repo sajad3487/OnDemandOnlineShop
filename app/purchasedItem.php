7fab2473d9526f132c6f162b87eb58b793123719
sajad sharif <sharif.sajad@gmail.com>
2020-05-30 14:51:47 +0430

    Initial commit

---
+++ app/purchasedItem.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class purchasedItem extends Model
+{
+    //
+}
---
+++ app/purchasedPo.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class purchasedPo extends Model
+{
+    //
+}
