d89c2f029eda5cd5244357575315fc32b5219e59
sajad sharif <sharif.sajad@gmail.com>
2020-06-05 16:15:08 +0430

    test windows push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Test
+                    Tessst
                 </div>
 
                 <div class="links">
