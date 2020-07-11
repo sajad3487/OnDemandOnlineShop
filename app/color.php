da9774fe0c5fe29ad0852ac12526111bf04386dc
sajad971 <sharif.sajad@gmail.com>
2020-07-11 08:15:28 +0430

    start adding color

--- app/color.php
+++ app/color.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class color extends Model
 {
-    //
+    public function product (){
+        return $this->belongsToMany(product::class);
+    }
 }
--- app/product.php
+++ app/product.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class product extends Model
 {
     public $guarded=[];
+
+    public function color (){
+        return $this->belongsToMany(color::class);
+    }
 }
